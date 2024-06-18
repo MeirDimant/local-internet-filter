3700dc91b1bc6b4920a0a717a4717a0314b0640f
MeirDi <321mid@gmail.com>
2024-06-18 13:05:32 +0300

    comments for plugin base

--- core/plugin_base.py
+++ core/plugin_base.py
@@ -6,10 +6,13 @@ from core.iflow import IFlow
 class PluginBase(metaclass=SingletonABCMeta):
     @abstractmethod
     def title() -> str:
+        """Return the title of the plugin."""
         pass
 
     def onRequest(self, flow: IFlow) -> bool:
+        """Process the request. Return False to stop further processing."""
         pass
 
     def onResponce(self, flow: IFlow) -> bool:
+        """Process the response. Return False to stop further processing."""
         pass
